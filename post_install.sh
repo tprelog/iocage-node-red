c2ebb28961b87801b5afb386cda1696deedbe5f7
Troy Prelog <35702532+tprelog@users.noreply.github.com>
2022-02-09 21:40:20 -0500

    update scripts

--- post_install.sh
+++ post_install.sh
@@ -7,7 +7,6 @@ usr_home=/var/db/nodered
 install -d -g ${usr_id} -o ${usr_id} -m 775 -- ${usr_home}
 pw useradd -u ${usr_id} -n ${usr_name} -d ${usr_home} -s /usr/local/bin/bash -w no
 
-export PYTHON="/usr/local/bin/python2.7"
 /usr/local/bin/npm install -g --unsafe-perm node-red
 
 sysrc -f /etc/rc.conf nodered_enable="YES"
